6ce1ab734928dc57837688033acc76268f2fe58b
a01663094-jpg <a01663094@tec.mx>
2026-05-08 13:47:28 -0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -33,7 +33,7 @@ def change(x, y):
 
 def inside(head):
     """Return True if head is inside boundaries."""
-   return -190 < head.x < 190 and -190 < head.y < 190
+  return -200 < head.x < 190 and -200 < head.y < 190
 
 
 def move_food():
